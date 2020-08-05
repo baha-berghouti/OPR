a226de622db69fee8f52e3db0720d3e2648a58cf
baha-berghouti <62513376+baha-berghouti@users.noreply.github.com>
2020-08-05 20:08:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -96,13 +96,14 @@ $(document).ready(function(){
   }
 // funcionality of adding and removing orders 
 function check(){
-// if($("#order").html()!=""){
+  if($("#checkIcon"+(x-1)).html()!=""){
+    $("#checkIcon"+(x-1)).html()=""
+  }
    document.querySelector("#checkIcon"+(x-1)).insertAdjacentHTML("afterbegin",'<svg id="check'+(x-1)+'" width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/></svg>'
    )
       document.querySelector(".size"+(x-1)).disabled=true;
       document.querySelector(".select"+(x-1)).disabled=true;
       document.querySelector(".numberOf"+(x-1)).disabled=true;
-  // }
 }
 function uncheck(){
       $("#check"+(x-1)).remove();
